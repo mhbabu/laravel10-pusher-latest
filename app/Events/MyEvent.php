e7e7e903627a99f77c609b839e95f7534fc6dda5
mhbabu <write2babu@gmail.com>
2024-05-15 15:20:48 +0600

    Laravel Pusher integration done

--- app/Events/MyEvent.php
+++ app/Events/MyEvent.php
@@ -14,25 +14,6 @@ class MyEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
     public $message;
-    // /**
-    //  * Create a new event instance.
-    //  */
-    // public function __construct()
-    // {
-    //     //
-    // }
-
-    // /**
-    //  * Get the channels the event should broadcast on.
-    //  *
-    //  * @return array<int, \Illuminate\Broadcasting\Channel>
-    //  */
-    // public function broadcastOn(): array
-    // {
-    //     return [
-    //         new PrivateChannel('channel-name'),
-    //     ];
-    // }
 
     public function __construct($message)
     {
